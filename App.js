b8f75c75340704dd1a12458da93e7add27149422
nass <cabliveer@gmail.com>
2022-09-02 15:20:13 +0200

    cfs

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
- import { View, Text, StatusBar, SafeAreaView} from 'react-native'
+ import { View, Text, StatusBar, SafeAreaView, StatusBar} from 'react-native'
  import React from 'react'
 //import Home from './screens/Settings'
 import RootNavigation from './navigation'
@@ -6,7 +6,7 @@ import RootNavigation from './navigation'
  export default function App() {
    return (
     <SafeAreaView style={{
-      // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+       paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
       backgroundColor: "#eee",
       flex: 1
     }}>
