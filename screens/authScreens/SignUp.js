f3858449e41f079f079ef2239f2f318e0dc859a7
nass <cabliveer@gmail.com>
2022-09-09 01:22:36 +0200

    vd

--- screens/authScreens/SignUp.js
+++ screens/authScreens/SignUp.js
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, StatusBar, Image, TextInput, StyleSheet, TouchableOpacity, Alert } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { Entypo, MaterialIcons } from '@expo/vector-icons'
-import { addUser, auth } from '../../firebase'
+import { addRestaurant, addUser, auth } from '../../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { LinearGradient } from 'expo-linear-gradient'
 import * as Animatable from "react-native-animatable"
