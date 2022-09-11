d874d6f91547b5ddebb5b7845c4705477e6c8c00
nass <cabliveer@gmail.com>
2022-09-11 20:54:11 +0200

    fs

--- components/SettingsComponent.js
+++ components/SettingsComponent.js
@@ -1,18 +1,18 @@
-import { View, Text, SafeAreaView, StatusBar, Image, TextInput, StyleSheet, TouchableOpacity, Pressable} from 'react-native'
-import React, {useState, useEffect, useContext} from 'react'
+import { View, Text, SafeAreaView, StatusBar, Image, TextInput, StyleSheet, TouchableOpacity, Pressable } from 'react-native'
+import React, { useState, useEffect, useContext } from 'react'
 import { Entypo, FontAwesome5, MaterialIcons } from '@expo/vector-icons'
-import { auth, getDriverInfos, updateRestaurantInfos} from '../firebase'
-import { signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth'
+import { auth, getDriverInfos, updateRestaurantInfos } from '../firebase'
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { RestaurantContext } from '../context/RestaurantContext'
 import { ScrollView } from 'react-native-gesture-handler'
 import SearchBar from './SearchBar'
 
-export default function SettingsComponent({navigation, bs}) {
+export default function SettingsComponent({ navigation, bs }) {
 
- const {restaurantData, setRestaurantData} = useContext(RestaurantContext)
+  const { restaurantData, setRestaurantData } = useContext(RestaurantContext)
 
- //console.log(restaurantData)
+  //console.log(restaurantData)
 
   const [email, setEmail] = useState(restaurantData.email)
   const [name, setName] = useState(restaurantData.name)
@@ -20,11 +20,11 @@ export default function SettingsComponent({navigation, bs}) {
   const [address, setAddress] = useState(restaurantData.address)
   const [city, setCity] = useState(restaurantData.city)
 
-  
+
   // const [image, setImage] = useState(restaurantData.image_url)
   // const [password, setPassword] = useState('')
 
-  
+
 
   return (
     <View style={{
@@ -32,86 +32,89 @@ export default function SettingsComponent({navigation, bs}) {
       backgroundColor: "#e0ebeb",
       flex: 1
     }}>
-       <View style={{
-         alignItems: "center",
-         marginTop: 20
-       }}>
+      <View style={{
+        alignItems: "center",
+        marginTop: 20
+      }}>
         <Pressable onPress={
-        ()=>{
-            
-            bs.current.snapTo(0)}
-      }>
-             <Image 
-      //  source={{uri: restaurantData.image_url}} 
-      //  source={{uri: image}}
-      source={{uri: restaurantData.image}}
-       style={{
-         width: 100,
-         height: 100,
-         overflow: 'hidden',
-         borderRadius: 100 / 2,
-       }}
-       />
-       
+          () => {
+
+            bs.current.snapTo(0)
+          }
+        }>
+          <Image
+            //  source={{uri: restaurantData.image_url}} 
+            //  source={{uri: image}}
+            source={{ uri: restaurantData.image }}
+            style={{
+              width: 100,
+              height: 100,
+              overflow: 'hidden',
+              borderRadius: 100 / 2,
+            }}
+          />
+
         </Pressable>
-      
-         
 
-       <Text style={{fontSize: 25, fontWeight: "bold", color: "#3d5c5c",
-      letterSpacing: 5}}>Upload Image</Text>
 
-       </View>
-       <View style={{
-         marginTop: 40
-       }}>
 
-         <View style={{marginHorizontal: 25}}>
-         <SearchBar style={{backgroundColor: "white", borderBottomColor: "grey", borderBottomWidth: 0.3}}
-         setAddress={setAddress}/>
-       </View>
+        <Text style={{
+          fontSize: 25, fontWeight: "bold", color: "#3d5c5c",
+          letterSpacing: 5
+        }}>Upload Image</Text>
 
-         <ScrollView> 
-         <View style={styles.textInputContainer}>
-         <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-           marginLeft: 6,
-         }}/>
-          <TextInput 
-          placeholder='Email' 
-          value={email}
-          onChangeText={(text)=>setEmail(text)}
-          style={styles.textInput}/>
-           
-         </View>
-         
-         <View style={styles.textInputContainer}>
-         <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-           marginLeft: 6,
-         }}/>
-          <TextInput 
-          placeholder='Name' 
-          value={name}
-          onChangeText={(text)=>setName(text)}
-          style={styles.textInput}/>
-           
-         </View>
+      </View>
+      <View style={{
+        marginTop: 40, flex: 1
+      }}>
 
-         
+        <View style={{ marginHorizontal: 25 }}>
+          <SearchBar style={{ backgroundColor: "white", borderBottomColor: "grey", borderBottomWidth: 0.3 }}
+            setAddress={setAddress} />
+        </View>
 
-         
+        <ScrollView>
+          <View style={styles.textInputContainer}>
+            <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='Email'
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              style={styles.textInput} />
 
-         <View style={styles.textInputContainer}>
-         <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
-           marginLeft: 6,
-         }}/>
-          <TextInput 
-          placeholder='Phone' 
-          value={phone}
-          onChangeText={(text)=>setPhone(text)}
-          style={styles.textInput}/>
-           
-         </View>
+          </View>
 
-         {/* <View style={styles.textInputContainer}>
+          <View style={styles.textInputContainer}>
+            <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='Name'
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={styles.textInput} />
+
+          </View>
+
+
+
+
+
+          <View style={styles.textInputContainer}>
+            <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='Phone'
+              value={phone}
+              onChangeText={(text) => setPhone(text)}
+              style={styles.textInput} />
+
+          </View>
+
+          {/* <View style={styles.textInputContainer}>
          <Entypo name="address" size={20} color="#3d5c5c" style={{
            marginLeft: 6,
          }}/>
@@ -123,21 +126,21 @@ export default function SettingsComponent({navigation, bs}) {
            
          </View> */}
 
-         <View style={styles.textInputContainer}>
-         <FontAwesome5 name="city" size={20} color="#3d5c5c" style={{
-           marginLeft: 6,
-         }}/>
-          <TextInput 
-          placeholder='City' 
-          value={city}
-          onChangeText={(text)=>setCity(text)}
-          style={styles.textInput}/>
-           
-         </View>
-        
-         
-         
-         {/* <View style={styles.textInputContainer}>
+          <View style={styles.textInputContainer}>
+            <FontAwesome5 name="city" size={20} color="#3d5c5c" style={{
+              marginLeft: 6,
+            }} />
+            <TextInput
+              placeholder='City'
+              value={city}
+              onChangeText={(text) => setCity(text)}
+              style={styles.textInput} />
+
+          </View>
+
+
+
+          {/* <View style={styles.textInputContainer}>
          <MaterialIcons name="person" size={20} color="#3d5c5c" style={{
            marginLeft: 6,
          }}/>
@@ -149,38 +152,38 @@ export default function SettingsComponent({navigation, bs}) {
            
          </View> */}
 
-         <TouchableOpacity  onPress={()=>{
-           updateRestaurantInfos(restaurantData.id, email, name, phone, address, city)
-         }}>
-           <View style={styles.button}>
-             <Text style={styles.buttonText}>Update</Text>
-           </View>
-         </TouchableOpacity>
-         
-         </ScrollView>
-       </View>
- 
+          <TouchableOpacity onPress={() => {
+            updateRestaurantInfos(restaurantData.id, email, name, phone, address, city)
+          }}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Update</Text>
+            </View>
+          </TouchableOpacity>
+
+        </ScrollView>
+      </View>
+
     </View>
-      
-     
+
+
   )
 }
 
 const styles = StyleSheet.create({
   textInputContainer: {
     flexDirection: "row",
-     
-     backgroundColor: "white",
-     marginHorizontal: 25,
-     
+
+    backgroundColor: "white",
+    marginHorizontal: 25,
+
     borderRadius: 5,
-     marginTop: 20,
-     alignItems: "center"
-     
-     
+    marginTop: 20,
+    alignItems: "center"
+
+
   },
   textInput: {
-    
+
     width: "90%",
     padding: 10
   },
