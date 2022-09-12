171ed35c1f481852b0c95d0fc133c435c4cec810
nass <cabliveer@gmail.com>
2022-09-12 06:39:25 +0200

    dg

--- components/AddInput.js
+++ components/AddInput.js
@@ -36,7 +36,7 @@ const AddInput = ({ title, inputs, setInputs}) => {
                             setInputs([...inputs.slice(0, index),
                                           {
                                           title: text,
-                                          ...inputs[index]
+                                        //   ...inputs[index]
                                         } ,
                                         ...inputs.slice(index + 1)])
                          }}/>
@@ -49,7 +49,7 @@ const AddInput = ({ title, inputs, setInputs}) => {
                             setInputs([...inputs.slice(0, index),
                                           {
                                           value: text,
-                                          ...inputs[index]
+                                        //   ...inputs[index]
                                         } ,
                                         ...inputs.slice(index + 1)])
                          }}
