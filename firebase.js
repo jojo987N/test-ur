50dda3aac599cd8f8db0ae9ff9a655201e329341
nass <cabliveer@gmail.com>
2022-09-19 21:46:27 +0200

    save

--- firebase.js
+++ firebase.js
@@ -139,11 +139,12 @@ export const getRestaurantId = (uid) => {
   const q = query(restaurantsCol, where('ownerId', '==', uid))
   return getDocs(q)
 }
-export const updateOrder = (order_Id, status, deliveryTime) => {
+export const updateOrder = (order_Id, status, cookingTime) => {
   const docRef = doc(db, 'orders', order_Id)
   return updateDoc(docRef, {
     status: status,
-    deliveryTime: deliveryTime
+    // deliveryTime: deliveryTime
+    cookingTime
   })
     .then(() => console.log('good'))
 }
