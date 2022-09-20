8e1b82579faf4662b81c736d32c47bd23aad8914
nass <cabliveer@gmail.com>
2022-09-20 04:06:22 +0200

    save

--- components/OrderCountDown.js
+++ components/OrderCountDown.js
@@ -18,8 +18,8 @@ export default function OrderCountDown({order, remainingTime, style}) {
       // initialRemainingTime={remainingTime}
       onUpdate={(remainingTime) => {
         // setRemainingTime(Math.round(remainingTime/60))
-        // updateRemainingTime(order.id, remainingTime)
-        console.log(remainingTime)
+         updateRemainingTime(order.id, remainingTime)
+        // console.log(remainingTime)
       }}
       onComplete={() => {
       }}
