4d00aed4e531df391a26eb659cb9cc38344f5fb1
nass <cabliveer@gmail.com>
2022-09-20 20:45:08 +0200

    save

--- components/ProgressSteps.js
+++ components/ProgressSteps.js
@@ -1,10 +1,18 @@
 import { View, Text, StyleSheet} from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AntDesign } from '@expo/vector-icons'
 import OrderCountDown from './OrderCountDown'
 
 export default function ProgressSteps({route, remainingTime}) {
-    const {order} = route.params
+     const {order} = route.params
+    const[orders, setOrders] = useState()
+    const [orderData, setOrderData] = useState()
+    useEffect(()=> {
+        onSnapshot(ordersCol, (snapshot)=>{
+            setOrderData(snapshot.docs.find(doc => doc.id === order.id).data())
+            // setOrders(snapshot.docs.map((doc)=> ({...doc.data(), id: doc.id })))
+        })
+    }, [])
   return (
     <View>
         <View style={styles.container1}>
@@ -15,7 +23,7 @@ export default function ProgressSteps({route, remainingTime}) {
          </View>
          <View style={{...styles.col, }}>
 
-             <OrderCountDown order={order} remainingTime={remainingTime} style={{backgroundColor: "white", height:49, color:"black"}}/>
+             <OrderCountDown order={orderData} remainingTime={remainingTime} style={{backgroundColor: "white", height:49, color:"black"}}/>
         </View>
          <View style={styles.col}>
              <View style={{height: 20, aspectRatio: 1, backgroundColor: "#e6e6e6", borderRadius: 50, borderColor:"grey", borderWidth:0.5}}></View>
