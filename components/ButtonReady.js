90fd3a2fd6ccd7b5e6ba323706c9510a4b34e410
nass <cabliveer@gmail.com>
2022-09-21 19:25:13 +0200

    save

--- components/ButtonReady.js
+++ components/ButtonReady.js
@@ -9,7 +9,7 @@ const ButtonReady = ({ order }) => {
         <TouchableOpacity style={styles.button}
             onPress={() =>{
                 updateStatus(order.id, APP_CONSTANT.READY)
-                .then(()=> navigation.navigate("OrderReadyDetails", { order }))
+                .then(()=> navigation.navigate("ReadyForPickup", { order }))
                  
             }}
         >
