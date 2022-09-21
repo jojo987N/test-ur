8892a3bb6d880c9094cfaf4c770c2b2ba8153eaf
nass <cabliveer@gmail.com>
2022-09-21 19:04:44 +0200

    save

--- screens/OrderDetails.js
+++ screens/OrderDetails.js
@@ -1,12 +1,13 @@
 import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 import React, {useState} from 'react'
 import { updateOrder } from '../firebase'
-import { APP_CONSTANT } from '../global'
+import { APP_CONSTANT, button } from '../global'
 import Menus from '../components/Menus'
 import Divider from '../components/Divider'
 import UserDetails from '../components/UserDetails'
 import ConfirmModal from '../components/ConfirmModal'
 import {ButtonFoodDone} from '../components/OrderReadyDetails'
+import ButtonReady from '../components/ButtonReady'
 
 export default function OrderDetails({route}) {
     const {order} = route.params 
@@ -18,34 +19,45 @@ export default function OrderDetails({route}) {
 
         <View style={styles.divider}></View>
         <Menus order={order}/>
-     { !route.params.orderStatus?<View style={styles.buttons}>
-          <TouchableOpacity onPress={()=>{
-           
-           setModalVisible(true)
-          }}>
-              <View style={{
-                  ...styles.button,
-                  backgroundColor: "#00ff00"
-              }}>
-                  <Text style={styles.text}>Confirm</Text>
-              </View>
-          </TouchableOpacity>
-          
-          <TouchableOpacity>
-              <View style={{
-                  ...styles.button,
-                  backgroundColor: "#cc0000",
-          
-              }}>
-                  <Text style={styles.text}>Decline</Text>
-              </View>
-          </TouchableOpacity>
-      </View>
-      : route.params.orderStatus === "inProgress"?
-      <ButtonFoodDone />:<></>}
+     
+    <RenderButton order={order} orderStatus={route.params.orderStatus} setModalVisible={setModalVisible}/>
       <ConfirmModal order={order} modalVisible={modalVisible} setModalVisible={setModalVisible}/>
     </View>
   )
+}
+
+const RenderButton = ({ orderStatus, order, setModalVisible}) => {
+    switch (orderStatus) {
+        case "inProgress":
+            return <ButtonReady order={order} orderStatus={orderStatus} />
+        default:
+            return <View style={styles.buttons}>
+                <TouchableOpacity onPress={() => {
+
+                    setModalVisible(true)
+                }}>
+                    <View style={{
+                        ...styles.button,
+                        backgroundColor: "#00ff00"
+                    }}>
+                        <Text style={styles.text}>{button.CONFIRM}</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <View style={{
+                        ...styles.button,
+                        backgroundColor: "#cc0000",
+
+                    }}>
+                        <Text style={styles.text}>{button.DECLINE}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            
+
+
+    }
 }
 
 const styles = StyleSheet.create({
